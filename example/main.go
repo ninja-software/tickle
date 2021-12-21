c0cd7fb8b80231ec72e3fe0f8bb94975629de002
Nathan Minissale <nathan@ninjasoftware.com.au>
2021-12-21 10:58:30 +0800

    Altered example with zero logger

--- example/main.go
+++ example/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/ninja-software/log_helpers"
 	"github.com/ninja-software/tickle"
+	"github.com/rs/zerolog"
 )
 
 var count int = 0
@@ -27,7 +28,7 @@ var sayMoo tickle.Task = func() (int, error) {
 		fmt.Println(arr[3])
 	}
 
-	log.Println("moo", count)
+	fmt.Println("moo", count)
 	return count, nil
 }
 
@@ -37,20 +38,27 @@ var recovery tickle.Recovery = func(err error) {
 }
 
 var clean tickle.Clean = func(dat interface{}, err error) {
-	log.Printf("moo %d is noooooo good\n", dat.(int))
+	fmt.Printf("moo %d is noooooo good\n", dat.(int))
 	// happen when count equals 3, 6, 9, ...
 }
 
 func main() {
+	log := log_helpers.LoggerInitZero("development")
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGKILL)
 	defer stop()
 
+	taskname := "sayMoo"
 	tickle.MinDurationOverride = true
 	tk := tickle.New(
-		"sayMoo", // task name
+		taskname, // task name
 		1,        // run every 10 second
 		sayMoo,   // run the sayMoo() function
 	)
+
+	// Override the standard logger
+	tkLogger := log_helpers.NamedLogger(log, "tickle").With().Str("task", taskname).Logger().Level(zerolog.DebugLevel)
+	tk.Log = &tkLogger
+	tk.LogVerboseMode = true
 
 	// do when sayMoo() returns error
 	tk.FuncClean = clean
--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module github.com/ninja-software/tickle
 go 1.15
 
 require (
+	github.com/ninja-software/log_helpers v1.0.0
 	github.com/ninja-software/terror v0.0.7
+	github.com/rs/zerolog v1.26.0
 )
